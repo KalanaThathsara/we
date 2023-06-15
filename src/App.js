c25b1bd849dbfc7534a88fb06f2699875fb872af
KalanaThathsara <k.t.nallaperuma96@gmail.com>
2023-06-15 09:44:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const api ={
+    key : '6150031b4e9906582f3b4a3ab25a0d8e',
+    base:'http://api.openweathermap.org/data/2.5/',
+  }
+
+  const[search, setSearch] = useState('');
+  const searchchClick = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((res) => res.json())
+    .then((result)=> {
+      console.log(result)
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* {Header} */}
+        <h1>Weather App</h1>
+
+        {/* Search Box */}
+        <div>
+        <input type='text' 
+        placeholder='Enter City / Town...'
+        onChange={(e) => setSearch(e.target.value)}></input>
+        <button onClick={searchchClick}>Search</button>
+        </div>
+        
+        
+        {/* Location  */}
+        <p>Colombo</p>
+
+
+        {/* Temparature Celcius */}
+        <p>30 c</p>
+
+        {/* Weather Condition - sunny, ... */}
+        <p>Raining</p>
       </header>
     </div>
   );
